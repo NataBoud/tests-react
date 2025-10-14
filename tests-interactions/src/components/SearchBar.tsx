f73d8e65010e33468495d5d60c9876b06c5beb14
Natalia Boudard <natalia.boudard@gmail.com>
2025-10-14 10:40:44 +0200

    fix(interactions): :zap: Ajout  de formulaire dans le composant

--- tests-interactions/src/components/SearchBar.tsx
+++ tests-interactions/src/components/SearchBar.tsx
@@ -12,31 +12,37 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
         setQuery(e.target.value);
     }
 
-    const handleSearch = () => {
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        // prevenir l'envoi du formulaire
+        e.preventDefault();
         const trimmedQuery = query.trim();
         if (trimmedQuery) {
             onSearch(trimmedQuery);
         }
     };
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === "Enter") {
-            handleSearch();
-        }
-    };
+    // const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    //     if (e.key === "Enter") {
+    //         handleSearch(e);
+    //     }
+    // };
 
     const isDisabled = query.trim().length === 0;
 
     return (
         <div>
-            <input
-                type="text"
-                placeholder="Rechercher..."
-                value={query}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-            />
-            <button onClick={handleSearch} disabled={isDisabled}>Rechercher</button>
+            <form onSubmit={handleSearch}>
+                <label htmlFor="search-input">Rechercher :</label>
+                <input
+                    type="text"
+                    placeholder="Rechercher..."
+                    value={query}
+                    onChange={handleChange}
+                    // onKeyDown={handleKeyDown}
+                />
+                <button type="submit" disabled={isDisabled}>Rechercher</button>
+            </form>
+
         </div>
     )
 }
