e594154a4484c3e4c02f038b800419b644ec454b
Natalia Boudard <natalia.boudard@gmail.com>
2025-10-15 09:25:21 +0200

    refactor(mock-global): :wastebasket: suppression des fichiers inutiles

--- tests-http-mock-global/src/tests/setup.ts
+++ tests-http-mock-global/src/tests/setup.ts
@@ -1,18 +1,1 @@
 import '@testing-library/jest-dom'
-import { afterAll, afterEach, beforeAll } from 'vitest'
-import { server } from '../../test/msw/server'
-
-// demarrer MSW avant les tests
-beforeAll(() => {
-    server.listen();
-});
-
-// reinitialiser les gestionnaires de requetes apres chaque test
-afterEach(() => {
-    server.resetHandlers();
-});
-
-// arreter MSW apres les tests
-afterAll(() => {
-    server.close();
-});
